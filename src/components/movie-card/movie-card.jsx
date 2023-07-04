67e1017151dacb1313965a4fb44afe17976c0508
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-07-03 21:55:19 -0400

    Updated code for favorites and added genre and director to Proptypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,18 +2,20 @@ import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-export const MovieCard = ({ movie }) => {
+export const MovieCard = ({ movie, onAddToFavorites }) => {
+    const handleAddToFavorites = () => {
+        onAddToFavorites(movie.id);
+    };
+
     return (
-        <Card>
+        <Card
+            as={Link}
+            to={`/movies/${encodeURIComponent(movie.id)}`}
+            className="text-decoration-none h-100"
+        >
             <Card.Img variant="top" src={movie.image} />
             <Card.Body>
                 <Card.Title>{movie.title}</Card.Title>
-                <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
-                    <Button variant="link">Open</Button>
-                </Link>
-                <Link to={`/profile`}>
-                    <Button variant="link">Favorite</Button>
-                </Link>
             </Card.Body>
         </Card>
     );
@@ -22,8 +24,20 @@ export const MovieCard = ({ movie }) => {
 MovieCard.propTypes = {
     movie: PropTypes.shape({
         title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        genre: PropTypes.shape({
+            name: PropTypes.string,
+            description: PropTypes.string
+        }),
+        director: PropTypes.shape ({
+            name: PropTypes.string,
+            bio: PropTypes.string,
+            birthYear: PropTypes.string,
+            deathYear: PropTypes.string
+        }),
         image: PropTypes.string.isRequired,
         year: PropTypes.string,
         rating: PropTypes.string
-    }).isRequired
+    }).isRequired,
+    addToFavorites: PropTypes.func.isRequired
 };
